d35e6ac688916cb3e0c1099905e1bb8ea2847c1c
Vladimir <bahv-240298@mail.ru>
2021-10-03 22:53:07 +0300

    Add more material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nail-app';
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry
+      .addSvgIcon('back',this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/back.svg"))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,9 @@ import { TopComponent } from './common/top/top.component';
 import {MatSelectModule} from "@angular/material/select";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {HttpClientModule} from "@angular/common/http";
 
 const appRoutes : Routes = [
   {path: '', component: AppComponent},
@@ -68,6 +71,9 @@ const appRoutes : Routes = [
     MatOptionModule,
     MatInputModule,
     MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [
     ClientsService,
