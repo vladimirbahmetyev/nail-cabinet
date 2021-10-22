678a069f21899db29e52cb5a82c99b04d47099d3
Vladimir <bahv-240298@mail.ru>
2021-10-22 15:45:14 +0300

    Added validation to create service page

--- src/app/record-page/checkout/checkout.component.ts
+++ src/app/record-page/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { getWorksTime } from '../../utils/helpers';
 import { service, ServicesService } from '../../shared/servicesService/services.service';
 import { v4 } from 'uuid';
 import { RecordService } from '../../shared/recordService/record.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -11,18 +12,26 @@ import { RecordService } from '../../shared/recordService/record.service';
 })
 export class CheckoutComponent {
   @Output() onBack = new EventEmitter();
-  price = 0;
-  workTime = '1:00';
   workTimeSteps = getWorksTime();
-  constructor(private serviceService: ServicesService, private recordService: RecordService) {}
+  serviceForm: FormGroup;
+  constructor(
+    private serviceService: ServicesService,
+    private recordService: RecordService,
+    private fb: FormBuilder,
+  ) {
+    this.serviceForm = this.fb.group({
+      price: [null, [Validators.required, Validators.pattern('[0-9]*')]],
+      workTime: ['', [Validators.required]],
+    });
+  }
 
   onCreateService() {
     const id = v4();
     const recordId = this.recordService.selectedRecord.getValue()?.id || '';
     const service: service = {
       id: id,
-      price: this.price,
-      time: this.workTime,
+      price: this.serviceForm.value.price,
+      time: this.serviceForm.value.workTime,
       recordId: recordId,
     };
     this.serviceService.createService(service);
